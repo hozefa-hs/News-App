922d9689f9bf23be5a276c28da90522dd552b58a
Hozefa <hozefasailanawala786@gmail.com>
2024-06-27 20:18:15 +0530

    Splash Screen Added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:news_app_flutter/screens/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,71 +14,95 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => HomeScreen(),
-          ));
-    });
-  }
+  // void initState() {
+  //   super.initState();
+  //   Timer(Duration(seconds: 3), () {
+  //     Navigator.pushReplacement(
+  //         context,
+  //         MaterialPageRoute(
+  //           builder: (context) => HomeScreen(),
+  //         ));
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: CupertinoColors.systemBlue,
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(24),
-                      topRight: Radius.circular(4),
-                      bottomRight: Radius.circular(24),
-                      bottomLeft: Radius.circular(4),
-                    ),
-                  ),
-                  padding: EdgeInsets.all(12),
-                  child: Text(
-                    "Newsify",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: CupertinoColors.systemBlue,
-                    ),
-                  ),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/news_splash_image.png',
+            fit: BoxFit.cover,
+            height: 790,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                // color: Colors.black,
+                gradient: LinearGradient(
+                  colors: [Colors.black, Colors.transparent],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
                 ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(bottom: 30),
+          ),
+          Center(
+            child: Align(
+              alignment: Alignment.bottomCenter,
               child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
                   Text(
-                    "News from around the",
+                    "Stay Informed",
                     style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                    ),
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    "world for you.",
+                    "from Day One",
                     style: TextStyle(
-                      fontSize: 25,
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "Discover the latest News with us",
+                    style: TextStyle(
+                      fontSize: 16,
                       color: Colors.white,
                     ),
                   ),
+                  SizedBox(height: 15),
+                  Container(
+                    height: 50,
+                    width: 300,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomeScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
